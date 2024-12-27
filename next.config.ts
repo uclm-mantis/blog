f91f6561a35c9e554dba4a4741ba5082eaf6034f
Francisco Moya <francisco.moya@uclm.es>
2024-12-28 00:59:18 +0100

    Fixed static generation

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: isProd ? '/blog' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
