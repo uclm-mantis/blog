dd6f17686006756e7eec06a5e08592ed4b266a2e
Francisco Moya <francisco.moya@uclm.es>
2024-12-28 18:21:46 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,8 +22,8 @@ const config: Config = {
       },
       keyframes: {
         fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
         },
       },
     },
