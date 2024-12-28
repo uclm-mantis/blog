19c9a3f87c4dfc8e640ac26c005408f3c1228b8b
Francisco Moya <francisco.moya@uclm.es>
2024-12-28 18:37:18 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+const { basePath } = publicRuntimeConfig;
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={`${basePath}/favicon.ico`} />
       </Head>
       <body>
         <Main />
